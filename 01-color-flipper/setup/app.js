a5a68ef3772ca8fbbae64dec9be3fd28d5c890ba
mk1121 <marufm.3666.com>
2021-05-18 05:01:19 +0600

    successfully complete project-1 past part

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,12 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function(){
+    const randomNumber = colors[getRandomNumber()];
+    document.body.style.backgroundColor = randomNumber;
+    color.textContent = randomNumber;
+});
+function getRandomNumber(){
+   return Math.floor(Math.random() * colors.length);
+};
