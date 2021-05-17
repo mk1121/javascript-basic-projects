14c4ee88b329fe1f351e0ce7529ffd7b6c410558
mk1121 <marufm.3666.com>
2021-05-18 05:18:41 +0600

    successfully complete project 1

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,15 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+ 
+btn.addEventListener("click", function(){
+    let hexCol = "#";
+    for( let i = 0; i<6; i++){
+        hexCol += hex[getRandomNumber()];
+        color.textContent = hexCol;
+        document.body.style.backgroundColor = hexCol;
+    }
+});
+function getRandomNumber(){
+    return Math.floor(Math.random() * hex.length);
+};
